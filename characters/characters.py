72ce5072245d2d9e440f050f13d8987248c84b7a
David Smit <david.d.smit@gmail.com>
2019-09-07 10:51:45 -0400

    Working on the character class

--- characters/characters.py
+++ characters/characters.py
@@ -1,5 +1,8 @@
+import configuration.configuration
+import arcade
 class CharacterFactory:
     pass
 
-class Character:
-    pass
+class Character(arcade.Sprite):
+    def __init__(self):
+        super().__init__(self)
