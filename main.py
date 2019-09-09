1c28a6964a60b4c8ec523c88bf3b4cde37e98739
David Smit <david.d.smit@gmail.com>
2019-09-08 23:44:10 -0400

    lots of fun stuff!

--- main.py
+++ main.py
@@ -6,30 +6,22 @@ import modes
 #and any other menus that are needed. This file will also cordinate
 #between the different modes
 
-class GameInitialize(arcade.Window):
+class Game(arcade.Window):
     def __init__(self):
-        super().__init__(screen_width, screen_height, screen_titel)
+        super().__init__(screen_width, screen_height, screen_title)
         self.file_path = os.path.dirname(os.path.abspath(__file__))
-        self.game_mode = self.main_menu
-
-    def start_game(self):
-        self.frame_count = 0
-        self.all_sprites_list = arcade.SpriteList()
-        self.map_sprites_list = arcade.SpriteList()
-        self.character_sprites_list = arcade.SpriteList()
-        self.misc_sprites_list = arcade.SpriteList()
-
-        self.build_characters()
-        self.build_map()
-
-    def build_characters(self):
-        pass
-
-    def build_map(self):
-        pass
+        self.game_mode = modes.MainMenu()
+        self.game_mode.initiate()
 
     def on_draw(self):
         arcade.start_render()
 
+
 def main():
-    window = MyGam
+    window = Game()
+    window.start_game()
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
--- modes.py
+++ modes.py
@@ -3,10 +3,44 @@ import arcade
 #This file will contain the logic for the different game types.
 
 class Mode(arcade.Window):
+    def __init__(self):
+        self.characters = None
+        self.map = None
+    def initiate(self, characters, map):
+        self.characters = characters
+        self.map = map
+        self.build_characters()
+        self.build_map()
+        self.start()
+
+    def build_characters(self):
+        pass
+
+    def build_map(self):
+        pass
+
+    def start(self):
+        pass
+
+class MainMenu(Mode):
     pass
 
 class BattleMode(Mode):
-    pass
+    def start(self):
+        self.determine_turns()
+        self.turn(self.turns)
+
+    def determine_turns(self):
+        #cycle through characters and determine step order
+        self.turns = None
+
+    def turn(self):
+        pass
+
+    def next_turn(self):
+        self.turn_index += 1
+
+
 
 class ExploreMode(Mode):
     pass
